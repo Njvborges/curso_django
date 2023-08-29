7df413302462dc3218ed251c2311b194ea35fbdf
Njvborges <Njvborges@hotmail.com>
2023-08-29 19:23:13 +0100

    exemplo de template

--- projeto/settings.py
+++ projeto/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # We also have apps
+    'recipes',
 ]
 
 MIDDLEWARE = [
--- recipes/views.py
+++ recipes/views.py
@@ -1,10 +1,12 @@
-# from django.shortcuts import render
+from django.shortcuts import render
 from django.http import HttpResponse
 
 
 # Create your views here.
 def home(request):
-    return HttpResponse('home 2')
+    return render(request, 'recipes/home.html', context={
+        'name': 'Nuno Borges',
+    })
 
 
 def contacto(request):
